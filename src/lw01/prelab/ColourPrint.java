e7c8ff0e459a9a6e3e22aef0e865738f1a3707ee
Aditya Chandra-coder <kanggendong419@gmail.com>
2026-09-20 15:15:17 +0700

    Labwork 01 - Prelab

---
+++ src/lw01/prelab/ColourPrint.java
@@ -0,0 +1,24 @@
+public class ColourPrint extends PrintJob {
+
+    public ColourPrint(String id, int pages) {
+        super(id, pages);
+    }
+
+    @Override
+    public int calculateCharge() {
+        int pages = getPages();
+        int charge;
+        if (pages <= 10) {
+            charge = pages * 1500;
+        } else {
+            charge = (10 * 1500) + ((pages - 10) * 1000);
+        }
+        charge += 2000; // setup cost per complete copy
+        return charge;
+    }
+
+    @Override
+    public String label() {
+        return "Colour";
+    }
+}
